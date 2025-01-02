2e4817eb733b45b16f4301357b72f0a0ce2de5a0
medievalnudistofthedead <mnotd94@gmail.com>
2025-01-01 23:00:14 -0600

    Add battle script

---
+++ Plugins/[DBK_000] Deluxe Battle Kit/[002] Midbattle Scripting/QUESTION_BATTLE.rb
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# Question Battle created by Medi
+#-----------------------------------------------------------------------------
+module MidbattleScripts
+	QUESTION_BATTLE = {
+		"RoundStartCommand_foe_repeat" => {
+			"editSpeaker" => :Hide
+		},
+		"RoundEnd_foe_repeat" => {
+			"editSpeaker" => :Hide
+		},
+		"BattleEnd" => {
+			"editSpeaker" => :Hide
+		}
+	}
+end
